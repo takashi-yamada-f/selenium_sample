2e187712d97e3ac1ba5ae13d8228193da5f0bad1
yamada takashi <yamada@fonolink.co.jp>
2020-10-28 11:09:25 +0900

    docker設定ファイルとpythonファイルを登録

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # selenium_sample
-seleniumのサンプルスクリプトとdocker
+seleniumのサンプルスクリプトとdocker設定ファイル
+
+### PCにdockerをインストールし起動します。アカウント登録も必要です。
+https://www.docker.com/
+
+### git cloneしdockerコンテナを作成、起動します。
+```
+$ git clone https://github.com/takashi-yamada-f/selenium_sample.git
+$ cd selenium_sample
+$ docker-compose build
+$ docker-compose up -d
+```
+
+### ログインし、サンプルプログラムを起動します。
+```
+$ docker ps
+$ docker exec -it selenium_sample_python_app_1 bash
+# cd /scraping
+# python itell_headless.py 
+.
+----------------------------------------------------------------------
+Ran 1 test in 24.141s
+
+OK
+```
+
---
+++ python_app/src/itell_headless.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.set_window_size('1200', '1000')
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("https://itell.biz/")
+        time.sleep(3)
+        driver.find_element_by_id("search-area").click()
+        driver.find_element_by_id("search-area").clear()
+        driver.find_element_by_id("search-area").send_keys(u"金沢")
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        time.sleep(3)
+        self.assertEqual(u"表示エリア：金沢", driver.find_element_by_id("centerInfo").text)
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
+
